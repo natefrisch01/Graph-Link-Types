211e3f3bb074db931e2fbd6276212b10abc0d6ea
natefrisch01 <natefrisch01@gmail.com>
2024-01-07 14:29:14 -0500

    Added colors and function for writing colors. Currently the last color writeen overwrites all edges.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { Plugin, WorkspaceLeaf, Notice } from 'obsidian';
 import { getAPI } from 'obsidian-dataview';
 
 export default class UniqueMetadataKeysPlugin extends Plugin {
@@ -10,6 +10,11 @@ export default class UniqueMetadataKeysPlugin extends Plugin {
             id: 'print-unique-metadata-keys',
             name: 'Print Unique Metadata Keys',
             callback: () => this.printUniqueMetadataKeys()
+        });
+        this.addCommand({
+            id: 'print-graph-leaf',
+            name: 'Print Graph Leaf',
+            callback: () => this.findGraphLeaf()
         });
     }
 
@@ -28,10 +33,10 @@ export default class UniqueMetadataKeysPlugin extends Plugin {
 
         // Define a list of categorical colors as single integers
         const colors = [
-            9991159, 13948116, 16772608, 4280421, 16113345, 
-            9474192, 4617584, 15790320, 12320668, 16185078, 
-            32896, 15132410, 10040346, 16777128, 8388608, 
-            11184895, 8421376, 16761035, 255, 8421504
+            16343842, 7260435, 11226103, 8810003, 1997538, 
+            11796368, 9731429, 16177103, 15601550, 7601461, 
+            1066150, 6197085, 5122908, 1339852, 2975129, 
+            1364806, 3203221, 14122353, 7027020, 8280444
         ];
 
         // Map each unique key to a color
@@ -44,7 +49,10 @@ export default class UniqueMetadataKeysPlugin extends Plugin {
         this.keyColorMap.forEach((color, key) => console.log(`${key}: ${color}`));
     }
 
-    getColorForKey(key: string): number | undefined {
+    getColorForKey(key: string | null): number | undefined {
+        if (key === null) {
+            return 0;
+        }
         return this.keyColorMap.get(key);
     }
 
@@ -65,5 +73,35 @@ export default class UniqueMetadataKeysPlugin extends Plugin {
 
     isLink(value: any): boolean {
         return typeof value === 'object' && value.hasOwnProperty('path');
+    }
+
+    findGraphLeaf(): WorkspaceLeaf | null{
+        let graphLeaves = this.app.workspace.getLeavesOfType('graph');
+		if (graphLeaves.length != 1) {
+			if (graphLeaves.length < 1) {
+				new Notice('No graph view open');
+			} else {
+				new Notice('More than one graph view open, please choose an active one');
+			}
+			return null;
+		}
+        const links = graphLeaves[0].view.renderer.links;
+        
+        for (let i = 0; i < 2; i++) {
+            const sourceId = links[i].source.id;
+            const targetId = links[i].target.id;
+            console.log(sourceId);
+            console.log(targetId);
+            const linkKey = this.getMetadataKeyForLink(sourceId, targetId);
+            const linkColor = this.getColorForKey(linkKey);
+            console.log(linkColor);
+            console.log(links[i].renderer);
+            links[i].line._tintRGB = linkColor;
+            links[i].renderer.colors.line.rgb = links[i].line._tintRGB;
+            
+        }
+
+        
+		return graphLeaves[0]
     }
 }
